b96aa22d89bc359cad3db18832d40de08e309545
iNarrow12 <154646684+iNarrow12@users.noreply.github.com>
2025-04-13 21:41:21 -0700

    Add files via upload

---
+++ START-GAME.bat
@@ -0,0 +1,21 @@
+@echo off
+title Game XE Launcher
+color 0a
+
+echo -----------------------------------------
+echo        WELCOME TO: THE MINE
+echo      XE-Enhanced Launcher Boot
+echo -----------------------------------------
+timeout /t 1 >nul
+echo [XE Launcher]:Start Game XE Launcher...
+timeout /t 1 >nul
+echo [XE Launcher]:Running Game assets...
+timeout /t 1 >nul
+echo [XE Launcher]:Launching THE MINE GAME...
+timeout /t 2 >nul
+echo [XE Launcher]:The Game Made By INarrow12...
+timeout /t 2 >nul
+echo [XE Launcher]:Launch Complete. Enjoy the experience;-)
+timeout /t 1 >nul
+echo Press any key to exit...
+pause >nul
